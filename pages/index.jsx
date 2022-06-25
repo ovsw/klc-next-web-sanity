860e5d05f27eba23c899d7a12b3ceb67f6ca8d26
OVIS <ovi@ovswebsites.com>
2022-06-25 08:30:30 +0300

    connected to Sanity, got blog posts

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,22 +1,31 @@
-import Home1Blog from "../src/components/wellcon/Home1/Home1Blog";
-import Home1Cta from "../src/components/wellcon/Home1/Hom1Cta";
-import PricingSection from "../src/components/Pricing";
+import Home1Blog from "src/components/wellcon/Home1/Home1Blog";
+import Home1Cta from "src/components/wellcon/Home1/Hom1Cta";
+import PricingSection from "src/components/Pricing";
 
-export default function Home(props) {
+import { getAllPosts } from "lib/api";
+
+export default function Home({ posts }) {
   // if (!story.content) {
   //   return <div>Loading...</div>;
   // }
 
   return (
     <>
+      {JSON.stringify(posts)}
       <PricingSection />
       <Home1Blog />
     </>
   );
 }
 
+// This function is called during the build (build time)
+// Provides props to your page
+// It will create static page
 export async function getStaticProps({ preview = false }) {
+  const posts = await getAllPosts();
   return {
-    props: {}, // will be passed to the page component as props
+    props: {
+      posts,
+    },
   };
 }
