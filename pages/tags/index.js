ad622d0fc2f9881c8f4af7f7ab4b06966fa3da40
OVIS <ovi@ovswebsites.com>
2022-07-18 12:34:05 +0300

    added root tags page placeholder

---
+++ pages/tags/index.js
@@ -0,0 +1,33 @@
+// import { NextSeo } from "next-seo";
+// import SEO from "../next-seo.config";
+import Link from "next/link";
+
+import {
+  getAllPosts,
+  getPostBySlug,
+  getAllPagesGeneric,
+  getAllPathsInfo,
+} from "lib/api";
+
+// import { useDebugValue } from "react";
+
+// import getOgImage from "../utils/getOgImageFromStory";
+
+export default function Page() {
+  return (
+    <>
+      {/* <Head>
+        <title>{story ? story.name : "My Site"}</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head> */}
+      {/* <NextSeo
+        title={story.content.seo_title}
+        description={story.content.seo_description}
+        canonical={`${SEO.openGraph.url}/${story.full_slug}`}
+        noindex={story.content.seo_noindex}
+        openGraph={ogContent}
+      /> */}
+      <p>Tag Page</p>
+    </>
+  );
+}
