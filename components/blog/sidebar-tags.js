5448b965db68cb5997fbf2a10bcddda2fb930a09
OVIS <ovi@ovswebsites.com>
2022-07-21 12:42:11 +0300

    fixed tags links in sidebar

--- components/blog/sidebar-tags.js
+++ components/blog/sidebar-tags.js
@@ -6,7 +6,7 @@ const SidebarTags = ({ heading = "Popular Tags", tags }) => {
       <h5 className="widget_title">{heading}</h5>
       <div className="tags">
         {tags?.map((tag, i) => (
-          <Link href={`/${tag.slug.current}`} key={i}>
+          <Link href={`/tags/${tag.slug.current}`} key={i}>
             {tag.title}
           </Link>
         ))}
