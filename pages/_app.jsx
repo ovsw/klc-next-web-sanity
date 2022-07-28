32f1ad03a842475077f1abbf2bfd2ee6045971bb
OVIS <ovi@ovswebsites.com>
2022-07-28 15:08:26 +0300

    installed framer motion

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,27 +1,28 @@
 import Head from "next/head";
 import Layout from "components/layout/Layout";
 
-// import StepsCustom from "../src/components/steps-custom";
-// import Home2Why from "../src/components/wellcon/home2/Home2Why";
-// import Home3About from "../src/components/wellcon/Home3/Home3About";
-// import PricingSection from "../src/components/Pricing";
-// import PackagesSection from "../src/components/PackagesSection";
-// import TestimonialsSection from "../src/components/Testimonials";
-// import PageTitleBanner from "../src/components/PageTitleBanner";
-// import PageBodySection from "../src/components/PageBodySection";
-// import PostBodySection from "../src/components/PostBodySection";
+import { LazyMotion, domAnimation, AnimatePresence } from "framer-motion";
 
 import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, router }) {
   return (
-    <Layout noHeaderTop sideBar headerStyle={1} absolute footerStyle={2}>
-      <Head>
-        <title>Climb Kids Life Coaching</title>
-        <link rel="icon" type="image/png" sizes="32x32" href="favicon.ico" />
-      </Head>
-      <Component {...pageProps} />
-    </Layout>
+    <LazyMotion features={domAnimation}>
+      <Layout noHeaderTop sideBar headerStyle={1} absolute footerStyle={2}>
+        {/* <Head>
+          <title>Climb Kids Life Coaching</title>
+          <link rel="icon" type="image/png" sizes="32x32" href="favicon.ico" />
+        </Head> */}
+        <AnimatePresence
+          exitBeforeEnter
+          onExitComplete={() => {
+            document.body.classList.remove("overflow-hidden");
+          }}
+        >
+          <Site Component={Component} pageProps={pageProps} router={router} />
+        </AnimatePresence>
+      </Layout>
+    </LazyMotion>
   );
 }
 
